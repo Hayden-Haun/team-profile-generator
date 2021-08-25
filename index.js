d10e36949bdcde2b64dcc02e22e90fc834bd4e9b
Hayden-Haun <haydenhaun@gmail.com>
2021-08-25 02:15:01 -0600

    all card types now render correctlygit add -A Added comments to files

--- index.js
+++ index.js
@@ -105,7 +105,6 @@ const engineerQuestions = [
 ];
 
 //  -----   FUNCTIONS   -----
-
 //  Takes data answered in manager prompt and creates a new Manager object. Appends this to the team array.
 function addIntern(data) {
   const newIntern = new Intern(data.name, data.id, data.email, data.special);
@@ -179,7 +178,7 @@ function addManager(data) {
   return newManager;
 }
 
-//Initialize Manager function: prompts user with manager questions and passes the data into addManager function
+//  Initialize Manager function: prompts user with manager questions and passes the data into addManager function
 function initManager() {
   inquirer.prompt(managerQuestions).then((data) => {
     const newEmployee = addManager(data);
@@ -205,43 +204,5 @@ function initManager() {
   });
 }
 
-//Call initialize function
+//  Call initialize function
 initManager();
-
-// // Hello Class!  here is strategy for attacking the team generator homework.  don't have to use it, but i love creating to do lists based on my pseducode
-
-// pseudocode out your strategy!!
-
-//
-// DONE -----1) build folder structure and install necessary libraries,  npm init -y, git init etc
-
-// DONE ----- 2) build 4 test files, Employee, Engineer, Intern, Manager....first time tests run they should fail, no code has been built yet,
-// DONE -----  think about what you need to test?
-// DONE -----  does a new object get created when you call the class?
-// DONE -----  does the name, email, id arguments get set as key/value pairs on the object
-// DONE ----- think about what class is the parent?  they are all employees and all will have name, id, email, engineer, intern, manager will be sub classes
-
-// DONE ----- 3) build out questions for team manager based on requirements
-// DONE ----- please build your team, what is your managers name?
-// DONE ----- what is the team managers id?
-// DONE ----- what is the team managers email?
-// DONE ----- what is the team managers office number?
-// DONE ----- which type of team member would you like to add?  Engineer, Intern, I dont want to add any more team members
-
-// DONE ------- test that a managers object was created from the class with the correct data!!!!
-
-// 4) create a base static HTML layout for the cards
-
-// 5) build out the manager card with manager object dynamically
-
-// 6) Next work on engineer object based on user input
-
-// 7) next work on  intern object based on user input
-
-// going to need a function that generates the html - you can use template literals  - have one js file for this, and export function to generate entire page
-
-// DONE ----- for the main index js file,  import all classes,  create an empty array for teamMembers and idArray,
-
-// grab input for manager and call that class to create an object,  then call another function to create team,  for this function use a switch statement to determine whether the user wants to create an engineer or intern and called the appropriate functions in cases.
-
-// finally, call a funciton if they dont want to create any more team members and call the function to render the HTML to build the team

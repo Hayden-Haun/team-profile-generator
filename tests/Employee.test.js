17ba69dcd28c139e9ab3e7d18ca54de23344e755
Hayden-Haun <haydenhaun@gmail.com>
2021-08-24 20:39:53 -0600

    Employee testing complete. 6 tests passes

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,19 +1,74 @@
+// const { it, expect } = require("@jest/globals");
+// const { describe } = require("yargs");
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
-  describe("test new object is created when calls", () => {
+  describe("new object should be created when called and have values equal to the input parameters", () => {
     it("should be created when called", () => {
+      //Arrange & Act
+      const newEmployee = new Employee("hayden-haun", "12345", "hayden@gmail");
+      const isCreated = newEmployee !== null;
+      //Assert
+      expect(isCreated).toEqual(true);
+      expect(typeof newEmployee).toEqual("object");
+    });
+
+    it("should have parameters equal to the input values", () => {
+      //Arrange
       const newName = "hayden-haun";
       const newId = "12345";
       const newEmail = "hayden@gmail";
-
+      //Act
       const newEmployee = new Employee(newName, newId, newEmail);
+      //Assert
+      expect(newEmployee.name).toEqual(newName);
+      expect(newEmployee.id).toEqual(newId);
+      expect(newEmployee.email).toEqual(newEmail);
+    });
+  });
 
-      const isCreated = newEmployee !== null;
+  describe("Each of the four methods should each return their respective key value", () => {
+    it(" should return object's name when getName is called", () => {
+      //Arrange
+      const newName = "hayden-haun";
+      const newId = "12345";
+      const newEmail = "hayden@gmail";
+      //Act
+      const newEmployee = new Employee(newName, newId, newEmail);
+      //Assert
+      expect(newEmployee.getName()).toEqual(newName);
+    });
 
-      expect(isCreated).toEqual(true);
+    it("should return object's id when getId is called", () => {
+      //Arrange
+      const newName = "hayden-haun";
+      const newId = "12345";
+      const newEmail = "hayden@gmail";
+      //Act
+      const newEmployee = new Employee(newName, newId, newEmail);
+      //Assert
+      expect(newEmployee.getId()).toEqual(newId);
+    });
 
-      expect(typeof newEmployee).toEqual("object");
+    it("should return object's email when getEmail is called", () => {
+      const newName = "hayden-haun";
+      const newId = "12345";
+      const newEmail = "hayden@gmail";
+      //Act
+      const newEmployee = new Employee(newName, newId, newEmail);
+      //Assert
+      expect(newEmployee.getEmail()).toEqual(newEmail);
+    });
+
+    it("should return object's role when getRole is called", () => {
+      //Arrange
+      const newName = "hayden-haun";
+      const newId = "12345";
+      const newEmail = "hayden@gmail";
+      //Act
+      const newEmployee = new Employee(newName, newId, newEmail);
+      //Assert
+      expect(newEmployee.getRole()).toEqual("Employee");
     });
   });
 });
